d2634afcefbc2b718ee740cce488efaa7a774325
bfeigin <ben.feigin@smartsheet.com>
2025-03-21 11:45:27 -0700

    fix why no tsc find?

--- lib/types/CreateOptions.ts
+++ lib/types/CreateOptions.ts
@@ -1,4 +1,4 @@
-import { ApiUrlPathBySection } from "./ApiUrls";
+import { ApiUrlPathByResource } from "./ApiUrls";
 import { CreateClientOptions } from "./CreateClientOptions";
 
 export type ClientOptions = Pick<
@@ -23,7 +23,7 @@ export type Requestor = {
 };
 
 export interface CreateOptions {
-  apiUrls: ApiUrlPathBySection;
+  apiUrls: ApiUrlPathByResource;
   requestor: Requestor;
   clientOptions?: ClientOptions;
 }
