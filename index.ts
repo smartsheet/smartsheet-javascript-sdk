5d9d9a733147b36b78f7711f7d820b1049699a87
smar-ben-feigin <152536304+smar-ben-feigin@users.noreply.github.com>
2025-03-27 10:05:36 -0700

    Add Lint/Prettier (#101)
    - Adds Eslint, Prettier, and Husky to help standardize code style within the repo.
    - Attempts to reasonably conform to existing patterns as often as possible.
    - Removes Gulp / jsHint
    - Updates development node version requirement (volta version pin) to node@20.19.0 to support modern tooling.

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import {CreateClient, CreateOptions} from "./lib/types";
-import { apiUrls } from "./lib/utils/apis";
-import { createEvents } from "./lib/events";
+import { CreateClient, CreateOptions } from './lib/types';
+import { apiUrls } from './lib/utils/apis';
+import { createEvents } from './lib/events';
 
-var _ = require('underscore');
-var winston = require('winston');
+const _ = require('underscore');
+const winston = require('winston');
 
 // Possible TODO: Namespace parameters for different subcomponents
 // E.g. clientOptions.requestor.instance OR
@@ -11,111 +11,110 @@ var winston = require('winston');
 //          w/ sub-paths maxRetryDurationSeconds and calcRetryBackoff
 
 function buildRequestor(clientOptions) {
-  if(clientOptions.requestor) return clientOptions.requestor;
+  if (clientOptions.requestor) return clientOptions.requestor;
 
-  var requestorConfig =
-    _.pick(clientOptions, 'maxRetryDurationSeconds', 'calcRetryBackoff');
+  const requestorConfig = _.pick(clientOptions, 'maxRetryDurationSeconds', 'calcRetryBackoff');
 
-  if(requestorConfig.maxRetryDurationSeconds)
+  if (requestorConfig.maxRetryDurationSeconds)
     requestorConfig.maxRetryDurationMillis = requestorConfig.maxRetryDurationSeconds * 1000;
 
   requestorConfig.logger = buildLogger(clientOptions);
 
-  return require('./lib/utils/httpRequestor.js')
-    .create(requestorConfig);
-};
+  return require('./lib/utils/httpRequestor.js').create(requestorConfig);
+}
 
 function buildLogger(clientOptions) {
-  if(hasMultipleLogOptions(clientOptions)) {
+  if (hasMultipleLogOptions(clientOptions)) {
     throw new Error(
-      "Smartsheet client options may specify at most one of " +
-      "'logger', 'loggerContainer', and 'logLevel'.");
+      'Smartsheet client options may specify at most one of ' + "'logger', 'loggerContainer', and 'logLevel'."
+    );
   }
 
-  if(clientOptions.logger) return clientOptions.logger;
+  if (clientOptions.logger) return clientOptions.logger;
 
-  if(clientOptions.logLevel) return buildLoggerFromLevel(clientOptions.logLevel);
+  if (clientOptions.logLevel) return buildLoggerFromLevel(clientOptions.logLevel);
 
-  if(clientOptions.loggerContainer) return buildLoggerFromContainer(clientOptions.loggerContainer);
+  if (clientOptions.loggerContainer) return buildLoggerFromContainer(clientOptions.loggerContainer);
 
   return null;
 }
 
 function hasMultipleLogOptions(clientOptions) {
-  return (clientOptions.logger && clientOptions.loggerContainer)
-  || (clientOptions.logger && clientOptions.logLevel)
-  || (clientOptions.loggerContainer && clientOptions.logLevel);
+  return (
+    (clientOptions.logger && clientOptions.loggerContainer) ||
+    (clientOptions.logger && clientOptions.logLevel) ||
+    (clientOptions.loggerContainer && clientOptions.logLevel)
+  );
 }
 
 function buildLoggerFromLevel(logLevel) {
-  if(winston.levels[logLevel] == null) {
+  if (winston.levels[logLevel] == null) {
     throw new Error(
       'Smartsheet client received configuration with invalid log level ' +
-      `'${logLevel}'. Use one of the standard Winston log levels.`);
+        `'${logLevel}'. Use one of the standard Winston log levels.`
+    );
   }
 
-  return new (winston.Logger)({
+  return new winston.Logger({
     transports: [
       new winston.transports.Console({
         level: logLevel,
         showLevel: false,
-        label: 'Smartsheet'
-      })
-    ]
+        label: 'Smartsheet',
+      }),
+    ],
   });
 }
 
 function buildLoggerFromContainer(container) {
-  if(container.has('smartsheet'))
-    return container.get('smartsheet');
+  if (container.has('smartsheet')) return container.get('smartsheet');
   else
     throw new Error(
-      "Smartsheet client received a logger container, but could not find a logger named " +
-      "'smartsheet' inside.");
+      'Smartsheet client received a logger container, but could not find a logger named ' + "'smartsheet' inside."
+    );
 }
 
-export const createClient: CreateClient = function(clientOptions) {
-  var requestor = buildRequestor(clientOptions);
+export const createClient: CreateClient = function (clientOptions) {
+  const requestor = buildRequestor(clientOptions);
 
-  var options: CreateOptions = {
+  const options: CreateOptions = {
     apiUrls: apiUrls,
     requestor: requestor,
     clientOptions: {
       accessToken: clientOptions.accessToken || process.env.SMARTSHEET_ACCESS_TOKEN,
       userAgent: clientOptions.userAgent,
-      baseUrl: clientOptions.baseUrl
-    }
+      baseUrl: clientOptions.baseUrl,
+    },
   };
 
   return {
-    constants  : require('./lib/utils/constants.js'),
-    contacts   : require('./lib/contacts/').create(options),
-    events     : createEvents(options),
-    favorites  : require('./lib/favorites/').create(options),
-    folders    : require('./lib/folders/').create(options),
-    groups     : require('./lib/groups/').create(options),
-    home       : require('./lib/home/').create(options),
-    images     : require('./lib/images/').create(options),
-    reports    : require('./lib/reports/').create(options),
-    request    : require('./lib/request/').create(options),
-    search     : require('./lib/search/').create(options),
-    server     : require('./lib/server/').create(options),
-    sheets     : require('./lib/sheets/').create(options),
-    sights     : require('./lib/sights/').create(options),
-    templates  : require('./lib/templates/').create(options),
-    tokens     : require('./lib/tokens/').create(options),
-    users      : require('./lib/users/').create(options),
-    webhooks   : require('./lib/webhooks/').create(options),
-    workspaces : require('./lib/workspaces/').create(options)
+    constants: require('./lib/utils/constants.js'),
+    contacts: require('./lib/contacts/').create(options),
+    events: createEvents(options),
+    favorites: require('./lib/favorites/').create(options),
+    folders: require('./lib/folders/').create(options),
+    groups: require('./lib/groups/').create(options),
+    home: require('./lib/home/').create(options),
+    images: require('./lib/images/').create(options),
+    reports: require('./lib/reports/').create(options),
+    request: require('./lib/request/').create(options),
+    search: require('./lib/search/').create(options),
+    server: require('./lib/server/').create(options),
+    sheets: require('./lib/sheets/').create(options),
+    sights: require('./lib/sights/').create(options),
+    templates: require('./lib/templates/').create(options),
+    tokens: require('./lib/tokens/').create(options),
+    users: require('./lib/users/').create(options),
+    webhooks: require('./lib/webhooks/').create(options),
+    workspaces: require('./lib/workspaces/').create(options),
   };
 };
 
 export const smartSheetURIs = {
   defaultBaseURI: 'https://api.smartsheet.com/2.0/',
   govBaseURI: 'https://api.smartsheetgov.com/2.0/',
-  euBaseURI: 'https://api.smartsheet.eu/2.0/'
-}
+  euBaseURI: 'https://api.smartsheet.eu/2.0/',
+};
 
-
-export { CreateClient, CreateClientOptions, SmartsheetClient } from "./lib/types";
-export * from "./lib/events/types"
+export { CreateClient, CreateClientOptions, SmartsheetClient } from './lib/types';
+export * from './lib/events/types';
