3972ef41a32e1b082ba9907142d1b233e69d374d
bfeigin <ben.feigin@smartsheet.com>
2025-05-30 10:55:44 -0700

    convert search

--- lib/search/index.ts
+++ lib/search/index.ts
@@ -1,39 +1,16 @@
-import type { CreateOptions, RequestCallback } from '../types';
+import { apiUrlByResource } from '../types';
+import type { CreateResourceProvider } from '../types/ApiResourceProvider';
 import type { SearchAllOptions, SearchApi, SearchResponse, SearchSheetOptions } from './types';
 
-export const createSearch = (options: CreateOptions): SearchApi => {
-  const requestor = options.requestor;
+const RESOURCE_PATH = apiUrlByResource['search'];
 
-  const optionsToSend = {
-    url: options.apiUrls.search,
-    urls: options.apiUrls,
-    ...options.clientOptions,
+export const createSearch: CreateResourceProvider<SearchApi> = (httpClient): SearchApi => {
+  const searchAll = (options: SearchAllOptions) => {
+    return httpClient.get<SearchAllOptions, SearchResponse>(RESOURCE_PATH, { params: options });
   };
 
-  const searchAll = (getOptions: SearchAllOptions, callback?: RequestCallback<SearchResponse>) => {
-    const options = {
-      ...optionsToSend,
-      ...getOptions,
-      queryParameters: {
-        ...getOptions.queryParameters,
-        query: getOptions.query,
-      },
-    };
-
-    return requestor.get(options, callback);
-  };
-
-  const searchSheet = (getOptions: SearchSheetOptions, callback?: RequestCallback<SearchResponse>) => {
-    const options = {
-      ...optionsToSend,
-      ...getOptions,
-      queryParameters: {
-        query: getOptions.query,
-        sheetId: getOptions.sheetId,
-      },
-    };
-
-    return requestor.get(options, callback);
+  const searchSheet = (options: SearchSheetOptions) => {
+    return httpClient.get<SearchSheetOptions, SearchResponse>(RESOURCE_PATH, { params: options });
   };
 
   return {
