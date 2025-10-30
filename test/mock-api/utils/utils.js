494917ee0cce5b6df96802013c4dfc2386a93f37
denislavstanchev <denislav.stanchev@smartsheet.com>
2025-10-30 13:22:43 +0200

    Try swap localhost with 127.0.0.1

--- test/mock-api/utils/utils.js
+++ test/mock-api/utils/utils.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 var smartsheet = require('../../../');
 
 exports.baseUrl = 'http://localhost:8082/2.0/';
-exports.wiremockUrl = 'http://localhost:8082';
+exports.wiremockUrl = 'http://127.0.0.1:8082';
 
 exports.createClient = function (baseUrl, accessToken) {
     return client = smartsheet.createClient({
