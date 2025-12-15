smartsheet/smartsheet-javascript-sdk

commit f9608efa3ab3b0c34b2548d56a95644881e7064d

convert the search module to typescript (#112)

lib/search/index.ts
@@ -1,0 +1,43 @@
+import type { CreateOptions, RequestCallback } from '../types';
+import type { SearchAllOptions, SearchApi, SearchResponse, SearchSheetOptions } from './types';
+
+export const createSearch = (options: CreateOptions): SearchApi => {
+  const requestor = options.requestor;
+
+  const optionsToSend = {
+    url: options.apiUrls.search,
+    urls: options.apiUrls,
+    ...options.clientOptions,
+  };
+
+  const searchAll = (getOptions: SearchAllOptions, callback?: RequestCallback<SearchResponse>) => {
+    const options = {
+      ...optionsToSend,
+      ...getOptions,
+      queryParameters: {
+        ...getOptions.queryParameters,
+        query: getOptions.query,
+      },
+    };
+
+    return requestor.get(options, callback);
+  };
+
+  const searchSheet = (getOptions: SearchSheetOptions, callback?: RequestCallback<SearchResponse>) => {
+    const options = {
+      ...optionsToSend,
+      ...getOptions,
+      queryParameters: {
+        query: getOptions.query,
+        sheetId: getOptions.sheetId,
+      },
+    };
+
+    return requestor.get(options, callback);
+  };
+
+  return {
+    searchAll,
+    searchSheet,
+  };
+};

lib/search/types.ts
@@ -1,0 +1,64 @@
+import type { RequestCallback } from '../types';
+import type { RequestOptions } from '../types/RequestOptions';
+
+export enum ParentResultType {
+  Workspace = 'workspace',
+  Sheet = 'sheet',
+  Folder = 'folder',
+}
+
+export enum SearchResultType {
+  Row = 'row',
+  Dashboard = 'dashboard',
+  Discussion = 'discussion',
+  Folder = 'folder',
+  Report = 'report',
+  Sheet = 'sheet',
+  SummaryField = 'summaryField',
+  Template = 'template',
+  Workspace = 'workspace',
+}
+
+export interface SearchResult {
+  // search result object id.
+  objectId: number;
+  // piece of text that is relevant to the query from the search result.
+  text: string;
+  // search result object type
+  objectType: SearchResultType;
+  // object id of the parent sheet, folder, or workspace that contains the search result.
+  parentObjectId: number;
+  //  name of the parent sheet, folder, or workspace of the search result.
+  parentObjectName: string;
+  // search result parent object type.
+  parentObjectType: ParentResultType;
+}
+
+export interface SearchResponse {
+  totalCount: number;
+  results: SearchResult[];
+}
+
+export interface SearchQueryParameters {
+  // when specified with the value of `personalWorkspace` it limits the response to only items in the user's personal workspace.
+  location?: string;
+  // when specified with a datetime in ms will only show results modified since that time.
+  modifiedSince?: string;
+  // when specified results will be limited to the passed scopes.
+  scopes?: SearchResultType[];
+}
+
+export interface SearchAllOptions extends RequestOptions<SearchQueryParameters, undefined> {
+  // text to search for
+  query: string;
+}
+
+export interface SearchSheetOptions extends SearchAllOptions {
+  // id of the sheet to search within.
+  sheetId: string;
+}
+
+export interface SearchApi {
+  searchAll: (options: SearchAllOptions, callback?: RequestCallback<SearchResponse>) => Promise<SearchResponse>;
+  searchSheet: (options: SearchSheetOptions, callback?: RequestCallback<SearchResponse>) => Promise<SearchResponse>;
+}

index.ts
@@ -1,6 +1,7 @@
 import type { CreateClient, CreateOptions } from './lib/types';
 import { apiUrls } from './lib/utils/apis';
 import { createEvents } from './lib/events';
+import { createSearch } from './lib/search';
 
 const _ = require('underscore');
 const winston = require('winston');
@@ -107,7 +108,7 @@ export const createClient: CreateClient = function (clientOptions) {
     images: require('./lib/images/').create(options),
     reports: require('./lib/reports/').create(options),
     request: require('./lib/request/').create(options),
-    search: require('./lib/search/').create(options),
+    search: createSearch(options),
     server: require('./lib/server/').create(options),
     sheets: require('./lib/sheets/').create(options),
     sights: require('./lib/sights/').create(options),

commit db51facb927368cd20b2adb2086a1b252b2779a5

Rever search breaking change introduced with https://github.com/smartsheet/smartsheet-javascript-sdk/pull/112

lib/search/index.ts
@@ -1,7 +1,7 @@
-import type { RequestCallback } from '../types/RequestCallback';
+import type { RequestCallback, RequestOptions } from '../types';
 import type { CreateOptions } from '../types/CreateOptions';
 import { apiUrls } from '../utils/apis';
-import type { SearchAllOptions, SearchApi, SearchResponse, SearchSheetOptions } from './types';
+import type { SearchQueryParameters, SearchApi, SearchResponse, SearchSheetOptions } from './types';
 
 export const createSearch = (options: CreateOptions): SearchApi => {
   const requestor = options.requestor;
@@ -12,30 +12,17 @@ export const createSearch = (options: CreateOptions): SearchApi => {
     ...options.clientOptions,
   };
 
-  const searchAll = (getOptions: SearchAllOptions, callback?: RequestCallback<SearchResponse>) => {
-    const options = {
-      ...optionsToSend,
-      ...getOptions,
-      queryParameters: {
-        ...getOptions.queryParameters,
-        query: getOptions.query,
-      },
-    };
-
+  const searchAll = (
+    getOptions: RequestOptions<SearchQueryParameters, undefined>,
+    callback?: RequestCallback<SearchResponse>
+  ) => {
+    const options = { ...optionsToSend, ...getOptions };
     return requestor.get(options, callback);
   };
 
   const searchSheet = (getOptions: SearchSheetOptions, callback?: RequestCallback<SearchResponse>) => {
-    const options = {
-      ...optionsToSend,
-      ...getOptions,
-      queryParameters: {
-        query: getOptions.query,
-      },
-    };
-
+    const options = { ...optionsToSend, ...getOptions };
     options.url = apiUrls.search + '/sheets/' + getOptions.sheetId;
-
     return requestor.get(options, callback);
   };
 

lib/search/types.ts
@@ -20,12 +20,18 @@ export enum SearchResultType {
 }
 
 export interface SearchResult {
+  // array of context data strings relevant to the search query.
+  contextData: string[];
+  // favorite indicates whether the search result is marked as a favorite by the user.
+  favorite: boolean;
   // search result object id.
   objectId: number;
   // piece of text that is relevant to the query from the search result.
   text: string;
   // search result object type
   objectType: SearchResultType;
+  // indicates whether the parent object of the search result is marked as a favorite by the user.
+  parentObjectFavorite: boolean;
   // object id of the parent sheet, folder, or workspace that contains the search result.
   parentObjectId: number;
   //  name of the parent sheet, folder, or workspace of the search result.
@@ -40,25 +46,32 @@ export interface SearchResponse {
 }
 
 export interface SearchQueryParameters {
+  // text to search for
+  query: string;
   // when specified with the value of `personalWorkspace` it limits the response to only items in the user's personal workspace.
   location?: string;
   // when specified with a datetime in ms will only show results modified since that time.
   modifiedSince?: string;
   // when specified results will be limited to the passed scopes.
-  scopes?: SearchResultType[];
+  scopes?: string;
+  // When specified with a value of favoriteFlag, response indicates which returned items are favorites. favorite -- dashboards, folders, reports, sheets, templates, and workspaces will have the property favorite: true parentObjectFavorite -- attachments, discussions, summary fields, and rows will have the property parentObjectFavorite: true
+  include?: string;
 }
 
-export interface SearchAllOptions extends RequestOptions<SearchQueryParameters, undefined> {
+export interface SearchSheetQueryParameters {
   // text to search for
   query: string;
 }
 
-export interface SearchSheetOptions extends SearchAllOptions {
+export interface SearchSheetOptions extends RequestOptions<SearchSheetQueryParameters, undefined> {
   // id of the sheet to search within.
   sheetId: string;
 }
 
 export interface SearchApi {
-  searchAll: (options: SearchAllOptions, callback?: RequestCallback<SearchResponse>) => Promise<SearchResponse>;
+  searchAll: (
+    options: RequestOptions<SearchQueryParameters, undefined>,
+    callback?: RequestCallback<SearchResponse>
+  ) => Promise<SearchResponse>;
   searchSheet: (options: SearchSheetOptions, callback?: RequestCallback<SearchResponse>) => Promise<SearchResponse>;
 }

test/mock-api/search/common_test_constants.ts
@@ -1,0 +1,53 @@
+// Common Search IDs
+export const TEST_SHEET_ID = 1234567890123456;
+export const TEST_WORKSPACE_ID = 9876543210987654;
+export const TEST_FOLDER_ID = 2345678901234567;
+export const TEST_REPORT_ID = 3456789012345678;
+export const TEST_TEMPLATE_ID = 4567890123456789;
+export const TEST_DASHBOARD_ID = 5678901234567890;
+export const TEST_ROW_ID = 7890123456789012;
+export const TEST_ROW_ID_2 = 7890123456789013;
+export const TEST_DISCUSSION_ID = 8901234567890123;
+export const TEST_ATTACHMENT_ID = 9012345678901234;
+
+// Common Search Names
+export const TEST_SHEET_NAME = 'Sample Sheet Name';
+export const TEST_WORKSPACE_NAME = 'Sample Workspace';
+export const TEST_FOLDER_NAME = 'Sample Folder Name';
+export const TEST_REPORT_NAME = 'Sample Report Name';
+export const TEST_TEMPLATE_NAME = 'Sample Template Name';
+export const TEST_DASHBOARD_NAME = 'Sample Dashboard Name';
+export const TEST_PROJECT_SHEET_NAME = 'Project Sheet';
+
+// Common Search Query Parameters
+export const TEST_QUERY = 'test search query';
+
+// Common Search Response Properties
+export const TEST_TOTAL_COUNT = 5;
+export const TEST_TOTAL_COUNT_REQUIRED = 2;
+export const TEST_TOTAL_COUNT_SHEET = 4;
+export const TEST_TOTAL_COUNT_SHEET_REQUIRED = 2;
+
+// Common Context Data
+export const TEST_CONTEXT_DATA_1 = 'Sample context data 1';
+export const TEST_CONTEXT_DATA_2 = 'Sample context data 2';
+export const TEST_FOLDER_CONTEXT = 'Sample folder context';
+export const TEST_REPORT_CONTEXT = 'Sample report context';
+export const TEST_TEMPLATE_CONTEXT = 'Sample template context';
+export const TEST_DASHBOARD_CONTEXT = 'Sample dashboard context';
+export const TEST_ROW_CONTEXT_1 = 'Cell data: Project milestone completed';
+export const TEST_ROW_CONTEXT_2 = 'Cell data: Task assigned to John';
+export const TEST_DISCUSSION_CONTEXT = 'Comment: Great progress on the project!';
+export const TEST_ATTACHMENT_CONTEXT = 'Attachment: project-plan.pdf';
+
+// Common Row Names
+export const TEST_ROW_NAME_1 = 'Row 5';
+export const TEST_ROW_NAME_2 = 'Row 12';
+export const TEST_DISCUSSION_NAME = 'Comment on Row 5';
+export const TEST_ATTACHMENT_NAME = 'project-plan.pdf';
+
+// Common Error Status Codes
+export const ERROR_500_STATUS_CODE = 500;
+export const ERROR_500_MESSAGE = 'Internal Server Error';
+export const ERROR_400_STATUS_CODE = 400;
+export const ERROR_400_MESSAGE = 'Malformed Request';

test/mock-api/search/search_all.spec.ts
@@ -1,0 +1,229 @@
+import crypto from 'crypto';
+import { createClient, findWireMockRequest } from '../utils/utils';
+import { expect } from '@jest/globals';
+import { SearchResultType } from '@smartsheet/search/types';
+import {
+    TEST_SHEET_ID,
+    TEST_WORKSPACE_ID,
+    TEST_FOLDER_ID,
+    TEST_REPORT_ID,
+    TEST_TEMPLATE_ID,
+    TEST_DASHBOARD_ID,
+    TEST_SHEET_NAME,
+    TEST_WORKSPACE_NAME,
+    TEST_FOLDER_NAME,
+    TEST_REPORT_NAME,
+    TEST_TEMPLATE_NAME,
+    TEST_DASHBOARD_NAME,
+    TEST_QUERY,
+    TEST_TOTAL_COUNT,
+    TEST_TOTAL_COUNT_REQUIRED,
+    TEST_CONTEXT_DATA_1,
+    TEST_CONTEXT_DATA_2,
+    TEST_FOLDER_CONTEXT,
+    TEST_REPORT_CONTEXT,
+    TEST_TEMPLATE_CONTEXT,
+    TEST_DASHBOARD_CONTEXT,
+    ERROR_500_STATUS_CODE,
+    ERROR_500_MESSAGE,
+    ERROR_400_STATUS_CODE,
+    ERROR_400_MESSAGE
+} from './common_test_constants';
+
+describe('Search - searchAll endpoint tests', () => {
+    const client = createClient();
+
+    const TEST_LOCATION = 'personalWorkspace';
+    const TEST_MODIFIED_SINCE = '2024-01-01T00:00:00Z';
+    const TEST_INCLUDE = 'favoriteFlag';
+    const TEST_SCOPES = [SearchResultType.Sheet, SearchResultType.Folder, SearchResultType.Report].join(',');
+
+    it('searchAll generated url is correct', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            queryParameters: {
+                query: TEST_QUERY,
+                location: TEST_LOCATION,
+                modifiedSince: TEST_MODIFIED_SINCE,
+                include: TEST_INCLUDE,
+                scopes: TEST_SCOPES
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/search/search-all/all-response-body-properties'
+            }
+        };
+        await client.search.searchAll(options);
+        const matchedRequest = await findWireMockRequest(requestId);
+
+        const parsedUrl = new URL(matchedRequest.absoluteUrl);
+        expect(parsedUrl.pathname).toEqual('/2.0/search');
+
+        const queryParamsObject = Object.fromEntries(parsedUrl.searchParams);
+        expect(queryParamsObject).toEqual({
+            query: TEST_QUERY,
+            location: TEST_LOCATION,
+            modifiedSince: TEST_MODIFIED_SINCE,
+            include: TEST_INCLUDE,
+            scopes: TEST_SCOPES
+        });
+    });
+
+    it('searchAll all response body properties', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/search/search-all/all-response-body-properties'
+            }
+        };
+        const response = await client.search.searchAll(options);
+
+        expect(response).toEqual({
+            results: [
+                {
+                    contextData: [
+                        TEST_CONTEXT_DATA_1,
+                        TEST_CONTEXT_DATA_2
+                    ],
+                    favorite: true,
+                    objectId: TEST_SHEET_ID,
+                    objectType: 'sheet',
+                    parentObjectFavorite: false,
+                    parentObjectId: TEST_WORKSPACE_ID,
+                    parentObjectName: TEST_WORKSPACE_NAME,
+                    parentObjectType: 'workspace',
+                    text: TEST_SHEET_NAME
+                },
+                {
+                    contextData: [
+                        TEST_FOLDER_CONTEXT
+                    ],
+                    favorite: false,
+                    objectId: TEST_FOLDER_ID,
+                    objectType: 'folder',
+                    parentObjectFavorite: true,
+                    parentObjectId: TEST_WORKSPACE_ID,
+                    parentObjectName: TEST_WORKSPACE_NAME,
+                    parentObjectType: 'workspace',
+                    text: TEST_FOLDER_NAME
+                },
+                {
+                    contextData: [
+                        TEST_REPORT_CONTEXT
+                    ],
+                    favorite: true,
+                    objectId: TEST_REPORT_ID,
+                    objectType: 'report',
+                    parentObjectFavorite: false,
+                    parentObjectId: TEST_SHEET_ID,
+                    parentObjectName: TEST_SHEET_NAME,
+                    parentObjectType: 'sheet',
+                    text: TEST_REPORT_NAME
+                },
+                {
+                    contextData: [
+                        TEST_TEMPLATE_CONTEXT
+                    ],
+                    favorite: false,
+                    objectId: TEST_TEMPLATE_ID,
+                    objectType: 'template',
+                    text: TEST_TEMPLATE_NAME
+                },
+                {
+                    contextData: [
+                        TEST_DASHBOARD_CONTEXT
+                    ],
+                    favorite: true,
+                    objectId: TEST_DASHBOARD_ID,
+                    objectType: 'dashboard',
+                    parentObjectFavorite: true,
+                    parentObjectId: TEST_WORKSPACE_ID,
+                    parentObjectName: TEST_WORKSPACE_NAME,
+                    parentObjectType: 'workspace',
+                    text: TEST_DASHBOARD_NAME
+                }
+            ],
+            totalCount: TEST_TOTAL_COUNT
+        });
+    });
+
+    it('searchAll required response body properties', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/search/search-all/required-response-body-properties'
+            }
+        };
+        const response = await client.search.searchAll(options);
+
+        expect(response).toEqual({
+            results: [
+                {
+                    contextData: [
+                        'Sample context data'
+                    ],
+                    objectId: TEST_SHEET_ID,
+                    objectType: 'sheet',
+                    text: TEST_SHEET_NAME
+                },
+                {
+                    contextData: [
+                        TEST_FOLDER_CONTEXT
+                    ],
+                    objectId: TEST_FOLDER_ID,
+                    objectType: 'folder',
+                    text: TEST_FOLDER_NAME
+                }
+            ],
+            totalCount: TEST_TOTAL_COUNT_REQUIRED
+        });
+    });
+
+    it('searchAll error 500 response', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/errors/500-response'
+            }
+        };
+        try {
+            await client.search.searchAll(options);
+            expect(true).toBe(false); // Expected an error to be thrown
+        } catch (error) {
+            expect(error.statusCode).toBe(ERROR_500_STATUS_CODE);
+            expect(error.message).toBe(ERROR_500_MESSAGE);
+        }
+    });
+
+    it('searchAll error 400 response', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/errors/400-response'
+            }
+        };
+        try {
+            await client.search.searchAll(options);
+            expect(true).toBe(false); // Expected an error to be thrown
+        } catch (error) {
+            expect(error.statusCode).toBe(ERROR_400_STATUS_CODE);
+            expect(error.message).toBe(ERROR_400_MESSAGE);
+        }
+    });
+});

test/mock-api/search/search_sheet.spec.ts
@@ -1,0 +1,206 @@
+import crypto from 'crypto';
+import { createClient, findWireMockRequest } from '../utils/utils';
+import { expect } from '@jest/globals';
+import {
+    TEST_SHEET_ID,
+    TEST_ROW_ID,
+    TEST_ROW_ID_2,
+    TEST_DISCUSSION_ID,
+    TEST_ATTACHMENT_ID,
+    TEST_PROJECT_SHEET_NAME,
+    TEST_QUERY,
+    TEST_TOTAL_COUNT_SHEET,
+    TEST_TOTAL_COUNT_SHEET_REQUIRED,
+    TEST_ROW_CONTEXT_1,
+    TEST_ROW_CONTEXT_2,
+    TEST_DISCUSSION_CONTEXT,
+    TEST_ATTACHMENT_CONTEXT,
+    TEST_ROW_NAME_1,
+    TEST_ROW_NAME_2,
+    TEST_DISCUSSION_NAME,
+    TEST_ATTACHMENT_NAME,
+    ERROR_500_STATUS_CODE,
+    ERROR_500_MESSAGE,
+    ERROR_400_STATUS_CODE,
+    ERROR_400_MESSAGE
+} from './common_test_constants';
+
+describe('Search - searchSheet endpoint tests', () => {
+    const client = createClient();
+
+    it('searchSheet generated url is correct', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            sheetId: TEST_SHEET_ID.toString(),
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/search/search-sheet/all-response-body-properties'
+            }
+        };
+        await client.search.searchSheet(options);
+        const matchedRequest = await findWireMockRequest(requestId);
+
+        const parsedUrl = new URL(matchedRequest.absoluteUrl);
+        expect(parsedUrl.pathname).toEqual(`/2.0/search/sheets/${TEST_SHEET_ID}`);
+
+        const queryParamsObject = Object.fromEntries(parsedUrl.searchParams);
+        expect(queryParamsObject).toEqual({
+            query: TEST_QUERY
+        });
+    });
+
+    it('searchSheet all response body properties', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            sheetId: TEST_SHEET_ID.toString(),
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/search/search-sheet/all-response-body-properties'
+            }
+        };
+        const response = await client.search.searchSheet(options);
+
+        expect(response).toEqual({
+            results: [
+                {
+                    contextData: [
+                        TEST_ROW_CONTEXT_1
+                    ],
+                    favorite: false,
+                    objectId: TEST_ROW_ID,
+                    objectType: 'row',
+                    parentObjectFavorite: true,
+                    parentObjectId: TEST_SHEET_ID,
+                    parentObjectName: TEST_PROJECT_SHEET_NAME,
+                    parentObjectType: 'sheet',
+                    text: TEST_ROW_NAME_1
+                },
+                {
+                    contextData: [
+                        TEST_ROW_CONTEXT_2
+                    ],
+                    favorite: false,
+                    objectId: TEST_ROW_ID_2,
+                    objectType: 'row',
+                    parentObjectFavorite: true,
+                    parentObjectId: TEST_SHEET_ID,
+                    parentObjectName: TEST_PROJECT_SHEET_NAME,
+                    parentObjectType: 'sheet',
+                    text: TEST_ROW_NAME_2
+                },
+                {
+                    contextData: [
+                        TEST_DISCUSSION_CONTEXT
+                    ],
+                    favorite: true,
+                    objectId: TEST_DISCUSSION_ID,
+                    objectType: 'discussion',
+                    parentObjectFavorite: true,
+                    parentObjectId: TEST_SHEET_ID,
+                    parentObjectName: TEST_PROJECT_SHEET_NAME,
+                    parentObjectType: 'sheet',
+                    text: TEST_DISCUSSION_NAME
+                },
+                {
+                    contextData: [
+                        TEST_ATTACHMENT_CONTEXT
+                    ],
+                    favorite: false,
+                    objectId: TEST_ATTACHMENT_ID,
+                    objectType: 'attachment',
+                    parentObjectFavorite: true,
+                    parentObjectId: TEST_SHEET_ID,
+                    parentObjectName: TEST_PROJECT_SHEET_NAME,
+                    parentObjectType: 'sheet',
+                    text: TEST_ATTACHMENT_NAME
+                }
+            ],
+            totalCount: TEST_TOTAL_COUNT_SHEET
+        });
+    });
+
+    it('searchSheet required response body properties', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            sheetId: TEST_SHEET_ID.toString(),
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/search/search-sheet/required-response-body-properties'
+            }
+        };
+        const response = await client.search.searchSheet(options);
+
+        expect(response).toEqual({
+            results: [
+                {
+                    contextData: [
+                        TEST_ROW_CONTEXT_1
+                    ],
+                    objectId: TEST_ROW_ID,
+                    objectType: 'row',
+                    text: TEST_ROW_NAME_1
+                },
+                {
+                    contextData: [
+                        TEST_ROW_CONTEXT_2
+                    ],
+                    objectId: TEST_ROW_ID_2,
+                    objectType: 'row',
+                    text: TEST_ROW_NAME_2
+                }
+            ],
+            totalCount: TEST_TOTAL_COUNT_SHEET_REQUIRED
+        });
+    });
+
+    it('searchSheet error 500 response', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            sheetId: TEST_SHEET_ID.toString(),
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/errors/500-response'
+            }
+        };
+        try {
+            await client.search.searchSheet(options);
+            expect(true).toBe(false); // Expected an error to be thrown
+        } catch (error) {
+            expect(error.statusCode).toBe(ERROR_500_STATUS_CODE);
+            expect(error.message).toBe(ERROR_500_MESSAGE);
+        }
+    });
+
+    it('searchSheet error 400 response', async () => {
+        const requestId = crypto.randomUUID();
+        const options = {
+            sheetId: TEST_SHEET_ID.toString(),
+            queryParameters: {
+                query: TEST_QUERY
+            },
+            customProperties: {
+                'x-request-id': requestId,
+                'x-test-name': '/errors/400-response'
+            }
+        };
+        try {
+            await client.search.searchSheet(options);
+            expect(true).toBe(false); // Expected an error to be thrown
+        } catch (error) {
+            expect(error.statusCode).toBe(ERROR_400_STATUS_CODE);
+            expect(error.message).toBe(ERROR_400_MESSAGE);
+        }
+    });
+});