beab366ed063e539e4c4381775efe9fd27deeac8
Scott McGowan <scott.mcgowan@smartsheet.com>
2026-04-23 12:42:28 +0100

    feat: add create report support
    changing empty interface to type

--- lib/reports/types.ts
+++ lib/reports/types.ts
@@ -1620,10 +1620,7 @@ export interface CreateReportResult {
 /**
  * Options for creating a report.
  */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface CreateReportOptions extends RequestOptions<undefined, CreateReportBody> {
-  // No additional options required - body contains all necessary data
-}
+export type CreateReportOptions = RequestOptions<undefined, CreateReportBody>;
 
 /**
  * Response from creating a report.
